cfad7330f96fdbc7b76c802574ee666d3cd6c86f
Scherbmeister <scherbmeister@gmail.com>
2022-10-31 18:28:57 +0300

    Add port forwarding for WSL2

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dotfiles
+
+Dotfiles for WSL2 and stuff.
+
+## Small note
+
+If you run into error while cloning this repo over https run: 
+
+```sh
+
+# It's super not safe
+export GIT_SSL_NO_VERIFY=1
+
+```
+
+wsl_forward_ports.ps1 contains code to expose wsl ports to your local network if needed.
+
+
---
+++ wsl_forward_ports.ps1
@@ -0,0 +1,8 @@
+$ports = @(22, 80, 443, 8080)
+
+foreach ($port in $ports) {
+    netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=$port
+    netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=$port connectaddress=localhost connectport=$port
+}
+netsh interface portproxy show v4tov4
+
