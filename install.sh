9344eab7ea2b0db64ac16e3027e39956493058c7
Scherbmeister <scherbmeister@gmail.com>
2022-11-08 08:07:16 +0300

    Add: php istallation to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Disable hack for "git clone" over https error.
 export GIT_SSL_NO_VERIFY=0
 
+sudo add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt -y upgrade
 
@@ -17,3 +18,5 @@ git config --global user.email "scherbmeister@gmail.com"
 # Generate ssh key for github use etc.
 ssh-keygen -t ed25519 -C "scherbmeister@gmail.com"
 
+# Install PHP 
+sudo apt install php8.2 php8.2-cli php8.2-{curl,bz2,mbstring,intl,xdebug}
